808078b236be56c7aadde5294b7ac76b141e0651
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-18 19:32:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function threeSum(arr, target) {
-  //your code here
+  //your code here.
+	let ans = infinity;
+	for(let i= 0; i < arr.length; i++){
+		for(let j = i+1; j < arr.length; j++){
+			for(let k = j+1; k < arr.length; k++){
+				let t = arr[i]+arr[j]+arr[k];
+				if(Math.abs(target-ans) > Math.abs(x-t)){
+					ans = t;
+				}
+			}
+		}
+	}
+	return ans;
 }
 
 module.exports = threeSum;
