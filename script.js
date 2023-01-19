44108da762fc580a40be5d78be504af7fb4fef16
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-19 16:13:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function threeSum(arr, target) {
   //your code here.
-	let ans = infinity;
+	let ans = Number.MAX_VALUE;
 	for(let i= 0; i < arr.length; i++){
 		for(let j = i+1; j < arr.length; j++){
 			for(let k = j+1; k < arr.length; k++){
-				let t = arr[i]+arr[j]+arr[k];
+				let t = (arr[i]+arr[j]+arr[k]);
 				if(Math.abs(target-ans) > Math.abs(target-t)){
 					ans = t;
 				}
